0cf9f96e69bd010a9515b0dd20d8439b747b11b7
Ramiz3564 <ramiz3564@wrdsb.ca>
2020-10-06 16:04:07 +0000

    finished practice Q5 Oct 6

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+ import java.util.Scanner;
 /**
- *
- * @author 
+ * This program creates asterisks in a line according to how many the user wants
+ * @author Zach Ramirez 
  */
 public class Main {
 
@@ -9,7 +10,16 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a scanner for user input 
+    Scanner input = new Scanner(System.in); 
+    //ask user for a number between 1 - 10 
+    System.out.println("Please enter a number between 1 - 10"); 
+    int integer = input.nextInt(); 
+    //use loop to make asterisk line 
+    for(int count = 1; count <= integer; count++){
+      System.out.print("*"); 
+    } 
+
     
   }
 }
